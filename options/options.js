4a32b25b019e5f25796ea0b8b21e515eb10672b0
Crowdbotics <team@crowdbotics.com>
2022-12-08 19:19:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_swell_method_37834",
+  url: "https://the_swell_method_37834.botics.co",
+  api: "https://the_swell_method_37834.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
