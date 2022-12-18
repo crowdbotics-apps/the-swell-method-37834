b672057392d62964766ec6c16d316574170a05d6
Crowdbotics <team@crowdbotics.com>
2022-12-18 15:04:15 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,11 @@ export const globalOptions = {
   name: "the_swell_method_37834",
   url: "https://the_swell_method_37834.botics.co",
   api: "https://the_swell_method_37834.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
-  }
-};
+    copy: "Routes available!"
+  },
+  undefined: { styles: styles }
+}
